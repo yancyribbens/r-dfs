5ad59484e0fe7fe67491d846f496926bdfe60163
yancy <email@yancy.lol>
2023-09-23 12:22:40 +0200

    DFS in python

---
+++ dfs_recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.9
+
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = set()
+
+def dfs(visited, graph, node):
+	if node not in visited:
+		print(node)
+		visited.add(node)
+		for v in graph[node]:
+			dfs(visited, graph, v)
+
+dfs(visited, graph, '5')
