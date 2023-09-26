7bc87cd2fc38142222c70222a93c777a4ae46f86
yancy <email@yancy.lol>
2023-09-26 17:09:15 +0200

    Add iterative version

--- dfs_recurse.rs
+++ dfs_recurse.rs
@@ -1,37 +1,59 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-// Recursive depth first search (DFS).
-fn dfs(mut visited: &mut HashSet<usize>, graph: &HashMap<usize, Vec<usize>>, node: usize) {
-
-    // Check if the node has been visited.
+// Recursive depth first search.
+fn dfs(visited: &mut HashSet<usize>, graph: &HashMap<usize, Vec<usize>>, node: usize) {
+    // Check if the edge has been visited.
     if !visited.contains(&node) {
+        println!("visit {}", node);
 
         // It hasn't been visited, so mark it as visited now.
         visited.insert(node);
-        println!("{}", node);
 
-        // Get all children of this node.
+        // Get all edges of this vertex.
         let vertexs: &Vec<usize> = graph.get(&node).unwrap();
 
         // For each child, visit.
-        for v in vertexs {
+        for &v in vertexs {
 
             // Recusive call.
-            dfs(visited, graph, v.clone());
+            dfs(visited, graph, v);
+        }
+    }
+}
+
+// Iterative depth first search.
+fn dfs_iter(visited: &mut HashSet<usize>, graph: &HashMap<usize, Vec<usize>>, node: usize) {
+    let l = graph.len();
+
+    // The stack will never be larger than the graph. 
+    let mut s: Vec<usize> = Vec::with_capacity(l);
+    s.push(node);
+
+    while !s.is_empty() {
+        let v: usize = s.pop().unwrap();
+
+        // Check if the vertex has been visited.
+        if !visited.contains(&v) {
+            println!("visit {}", v);
+
+            // It hasn't been visited, so mark it as visited now.
+            visited.insert(v);
+
+            // Get all edges of this vertex.
+            let vertexs: &Vec<usize> = graph.get(&v).unwrap();
+
+            for &edge in vertexs {
+                s.push(edge);
+            }
         }
     }
 }
 
 // Entry point.
 fn main() {
-    // Set of visited (no duplicates).
-    let mut visited: HashSet<usize> = HashSet::new();
-
-    // A hashmap of tuples which represent a tree.  The first item is the node, 
-    // and the second item which is the Vector is the node's children.  
-    // In this case, the tree is a binary tree since each node has 
-    // 2 or less children.
+    // A hashmap of tuples which represent a graph.  The first item is the vertex, 
+    // and the second item which are edges.  
     let mut graph = HashMap::from([
         (5 , vec![3, 7]),
         (3 , vec![2, 4]),
@@ -39,8 +61,16 @@ fn main() {
         (2 , vec![]),
         (4 , vec![8]),
         (8 , vec![])
-]);
+    ]);
 
     // Start DFS at the root node 5.
+    // Set of visited (no duplicates).
+    let mut visited: HashSet<usize> = HashSet::new();
+    println!("start dfs recursive version");
     dfs(&mut visited, &mut graph, 5);
+
+    // Set of visited (no duplicates).
+    let mut visited: HashSet<usize> = HashSet::new();
+    println!("start dfs iterative version");
+    dfs_iter(&mut visited, &mut graph, 5);
 }
